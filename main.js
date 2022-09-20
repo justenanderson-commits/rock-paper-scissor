0720fb1f4c7fe0352cb1e64f61106815f698f96c
Justen Anderson <justen.anderson@gmail.com>
2022-09-20 17:09:44 -0600

    Plant the seedlings of the logic tree :)

---
+++ main.js
@@ -0,0 +1,9 @@
+var personWinCount;
+var computerWinCount;
+var rockPower;
+var paperPower;
+var scissorPower;
+
+var fighterArray = [// links to svgs, probably have to make an array of objects here.// ]
+
+// 
