51f983375f118a3b012d1fc394f693871c4f9531
Justen Anderson <justen.anderson@gmail.com>
2022-09-21 14:49:51 -0600

    Add initial JS, pseudocoding, and organizational comments.

--- game.js
+++ game.js
@@ -5,3 +5,11 @@ class Game {
 }
 
 module.exports = Game;
+
+// Game should include:
+// Two Player instances
+// A way to keep track of the data for the game board
+// A way to keep track of the selected game type
+// A way to check the Game’s board data for win conditions
+// A way to detect when a game is a draw (no one has won)
+// A way to reset the Game’s board to begin a new game
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Global variables -------------------------------------
 var personWinCount;
 var computerWinCount;
 var rockPower;
@@ -6,4 +7,18 @@ var scissorPower;
 
 var fighterArray = [// links to svgs, probably have to make an array of objects here.// ]
 
-// 
+//  Query Selectors ---------------------------------------
+
+
+
+// Event listeners ----------------------------------------
+
+
+// Functions (Single Responsibility Protocol)--------------
+
+
+// Problem solving process --------------------------------
+// console log everything
+// Re-read spec
+// talk it out loud
+// draw a picture
