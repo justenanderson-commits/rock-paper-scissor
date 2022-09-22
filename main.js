aa5ac7fc1a6036051c731fb42146125fea2d11f9
Justen Anderson <justen.anderson@gmail.com>
2022-09-22 00:06:36 -0600

    Add image assets and add to game logic.

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
 // Global variables -------------------------------------
-var personWinCount;
-var computerWinCount;
+// var personWinCount = humanPlayer.wins;
+// var computerWinCount = computerPlayer.wins;
 var rockPower;
 var paperPower;
 var scissorPower;
 
+
+// winCounter = [personWinCount, computerWinCount]
 var fighterArray = [] // links to svgs, probably have to make an array of objects here.// ]
 
 //  Query Selectors ---------------------------------------
 var classicGameCard = document.getElementById('card--game-classic');
 var spicyGameCard = document.getElementById('card--game-spicy');
-
-
-
+var changeGameCard = document.getElementById('change-game-selector');
+var headerInstructions = document.getElementById('text--instructions')
+var fighterArea = document.getElementById('container--fighter-area')
 
 // Event listeners ----------------------------------------
+window.addEventListener('load', showStartScreen);
 classicGameCard.addEventListener('click', loadClassicGame);
 spicyGameCard.addEventListener('click', loadSpicyGame);
+changeGameCard.addEventListener('click', showStartScreen);
 
 // Functions (Single Responsibility Protocol)--------------
 function show(element) {
@@ -26,18 +30,32 @@ function show(element) {
 function hide(element) {
   element.classList.add('hidden')
 
-}function loadClassicGame() {
+} function loadClassicGame() {
+  headerInstructions.innerText = 'Choose your fighter!'
   hide(classicGameCard)
   hide(spicyGameCard)
+  hide(changeGameCard)
   console.log('Classic game loading...')
 }
 
 function loadSpicyGame() {
+  headerInstructions.innerText = 'Choose your fighter!'
   hide(classicGameCard)
   hide(spicyGameCard)
+  hide(changeGameCard)
   console.log('Let\'s get spicy!')
 }
 
+function showStartScreen() {
+  show(classicGameCard)
+  show(spicyGameCard)
+// CHANGE to hide once the changeGameCard is fired by the game being run (and winner determined), wins++
+  show(changeGameCard)
+}
+
+//  Add headerInstructions.innerText = '💻 Computer won this round! 💻'
+//  Add headerInstructions.innerText = '🧟‍♂️ Human won this round! 🧟‍♂️'
+//  Add headerInstructions.innerText = '✍️ It's a draw! ✍️'
 
 
 // Problem solving process --------------------------------
